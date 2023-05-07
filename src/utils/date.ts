820205693cd618b98157cb1ad9c4b8b90ec246bf
lifeisegg123 <leejj2002@naver.com>
2023-05-07 22:46:48 +0900

    fix: date format

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,9 @@
 import { getDate, getMonth } from "date-fns";
 
+const padDate = (date: number) => date.toString().padStart(2, "0");
+
 export const getToday = () => {
   const now = new Date();
-  const today = `${getMonth(now) + 1}-${getDate(now)}`.padStart(5, "0");
+  const today = `${padDate(getMonth(now) + 1)}-${padDate(getDate(now))}`;
   return today;
 };
